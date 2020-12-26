7ff8b02e21a2b707e7316438dd03b9b06d7db918
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2020-12-26 05:51:54 +0100

    Add Login form

--- main.rkt
+++ main.rkt
@@ -28,7 +28,46 @@
        (shortcut-prefix (get-default-shortcut-prefix))
        (shortcut #\q)
        (callback (λ (item event)
-                   (display "On exit")
+                   (display "Exiting Application")
                    (send app show #f)))))
+
+(define main-view
+  (new vertical-panel%
+    (parent app)
+    (vert-margin 150)
+    (horiz-margin 300)))
+
+(define welcome-message
+  (new message%
+    (parent main-view)
+    (label "Sign up or Sign in")))
+
+(define login-form
+  (new vertical-pane%
+    (parent main-view)))
+
+(define user-name-field
+  (new text-field%
+    (label "Username")
+    (parent login-form)
+    (style '(single vertical-label))))
+
+(define user-password-field
+  (new text-field%
+    (label "Password")
+    (parent login-form)
+    (style '(single password vertical-label))))
+
+(define keep-logged-in-checkbox
+  (new check-box%
+    (parent login-form)
+    (label "Keep me logged in.")))
+
+(define login-btn
+  (new button%
+    (label "Login")
+    (parent login-form)
+    (callback (λ (item event)
+                (display "Validating login data.")))))
 
 (send app show #t)
